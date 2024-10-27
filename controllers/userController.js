81cf6f61f6cd09471405cb35dfc65630f0daec00
Vlasenko Oleksii <lesha.ku.vlasenko@gmail.com>
2024-10-27 13:55:54 +0200

    Play page (structure)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,15 @@
 exports.getHomePage = (req, res) => {
   res.render('pages/home');  
 };
+
+exports.getPlayPage = (req, res) => {
+  res.render('pages/play');  
+};
+
+exports.getTrainingPage = (req, res) => {
+  res.render('pages/training');  
+};
+
+exports.getLessonPage = (req, res) => {
+  res.render('pages/lesson');  
+};
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getHomePage);
+router.get('/play', userController.getPlayPage);
+router.get('/training', userController.getPlayPage);
+router.get('/lesson', userController.getPlayPage);
 
 module.exports = router;
